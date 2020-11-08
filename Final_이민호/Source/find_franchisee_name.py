8442a838f01f0b26fee2f9193dec6fa3d7becdf6
이민호 <lkjjr0424@hanmail.net>
2020-11-08 23:30:30 +0900

    delete old files

--- Final_이민호/Source/find_franchisee_name.py
+++
@@ -1,9 +0,0 @@
-#상호명 검색
-
-def find_franchisee(fr_list,cmp_nm):
-    find_list=[]
-    for i in fr_list:
-        name = i.cmpnm_nm.string #상호명 받아오기
-        if cmp_nm in name:
-            find_list.append(i)
-    return find_list
